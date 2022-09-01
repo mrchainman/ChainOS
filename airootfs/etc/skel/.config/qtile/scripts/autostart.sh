8a5add2e9c9d135925c38651e49282fef752e262
David Cimadom <david.cimadom@zone35.de>
2022-09-01 23:16:08 +0200

    environment

--- airootfs/etc/skel/.config/qtile/scripts/autostart.sh
+++ airootfs/etc/skel/.config/qtile/scripts/autostart.sh
@@ -2,6 +2,7 @@
 export PATH="/home/davidc/.local/bin:$PATH"
 /usr/lib/xfce-polkit/xfce-polkit &
 bg.sh &
+l.sh &
 picom -b &
 eww daemon &
 mkfifo /tmp/vol-icon && ~/.config/qtile/scripts/vol_icon.sh &
@@ -10,9 +11,9 @@ nm-applet &
 flameshot &
 # kitty --listen-on=unix:@neomutt --class="coms" -e "neomutt" &
 # kitty --listen-on=unix:@newsboat --class="coms" -e "newsboat" &
-# kitty --listen-on=unix:@ikhal --class="coms" -e "ikhal" &
+kitty --listen-on=unix:@ikhal --class="coms" -e "ikhal" &
 kitty --listen-on=unix:@btop --class="coms" -e "btop" &
 # kitty --listen-on=unix:@zabbix --class="mon" -e "zabbixmon" &
-# kitty --listen-on=unix:@spotify --class="Spotify" -e "spotify_player" &
+kitty --listen-on=unix:@spotify --class="Spotify" -e "spotify_player" &
 kitty --listen-on=unix:@neovim --class="neovim" -e "nvim" &
 qutebrowser &
