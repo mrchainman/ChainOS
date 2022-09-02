0bd2ed03343f0edd0aa8bee043fcaef06a8e6108
David Cimadom <david.cimadom@zone35.de>
2022-09-02 11:39:42 +0200

    changed autostarts

--- airootfs/etc/skel/.config/qtile/scripts/autostart.sh
+++ airootfs/etc/skel/.config/qtile/scripts/autostart.sh
@@ -7,10 +7,12 @@ picom -b &
 eww daemon &
 mkfifo /tmp/vol-icon && ~/.config/qtile/scripts/vol_icon.sh &
 dunst &
-nm-applet &
+snixembed --fork &
+iwgtk -i &
 flameshot &
-# kitty --listen-on=unix:@neomutt --class="coms" -e "neomutt" &
-# kitty --listen-on=unix:@newsboat --class="coms" -e "newsboat" &
+networkd-notify &
+kitty --listen-on=unix:@neomutt --class="coms" -e "neomutt" &
+kitty --listen-on=unix:@newsboat --class="coms" -e "newsboat" &
 kitty --listen-on=unix:@ikhal --class="coms" -e "ikhal" &
 kitty --listen-on=unix:@btop --class="coms" -e "btop" &
 # kitty --listen-on=unix:@zabbix --class="mon" -e "zabbixmon" &
